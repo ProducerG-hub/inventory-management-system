54bfb226e44ef144c35d76aed6697d5d3c702b6d
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-07-07 23:35:12 +0300

    Adding Lombok dependency for auto creation of getters, setters and constructores

--- inventory-management/src/main/java/com/inventory_management/entity/Category.java
+++ inventory-management/src/main/java/com/inventory_management/entity/Category.java
@@ -1,9 +1,17 @@
 package com.inventory_management.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name="categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
