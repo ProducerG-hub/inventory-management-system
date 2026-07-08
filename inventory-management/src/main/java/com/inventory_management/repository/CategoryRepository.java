1cbc4fa994dccdf90dd53b8de11e62c065a34ff6
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-07-08 20:18:23 +0300

    Created Entity Repositories which acts as the bridge between application and database

---
+++ inventory-management/src/main/java/com/inventory_management/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.inventory_management.repository;
+
+import com.inventory_management.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
---
+++ inventory-management/src/main/java/com/inventory_management/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.inventory_management.repository;
+
+import com.inventory_management.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
